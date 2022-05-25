f087049cfad381c4888467492a06c8fb3d7a6d7c
Jailton97 <jailton.cstper@gmail.com>
2022-05-24 21:48:05 -0300

    Players and Positions

---
+++ database/migrations/2022_05_24_234310_create_players_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->string(column: "name", length: 256);
+            $table->date(column: "birthday");
+            $table->foreignId(column: "position_id")->references("id")->on("positions");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+};
