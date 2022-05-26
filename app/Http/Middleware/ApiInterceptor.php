7723eeb5726a534a1fa031f5b00f1b14e963051e
Gabriel Mascarenhas <gabrielmascsousa@gmail.com>
2022-05-26 09:50:16 -0300

    Hotfix.

--- app/Http/Middleware/ApiInterceptor.php
+++ app/Http/Middleware/ApiInterceptor.php
@@ -17,6 +17,8 @@ class ApiInterceptor
     public function handle(Request $request, Closure $next)
     {
         $request->headers->set("Accept", "application/json");
-        return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
