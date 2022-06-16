bf6331d91f910ba016fb32c99d8c72b93d878795
Mateus Pena <mateus.18j@gmail.com>
2022-06-16 18:57:14 -0300

    Implementando o chaveamento da fase classificatoria.

--- app/Services/ClassificatoryConfrontationsService.php
+++ app/Services/ClassificatoryConfrontationsService.php
@@ -16,15 +16,39 @@ class ClassificatoryConfrontationsService
         $combinations = [];
         $size = $leagueTeams->count();
         for ($i = 0; $i < $size; $i++) {
-            $teamA = $leagueTeams->get($i)->name;
+            $teamA = $leagueTeams->get($i)->id;
             for ($j = $i+1; $j < $size; $j++) {
-                $teamB = $leagueTeams->get($j)->name;
-                $combinations[$teamA][] = $teamB;
-//                $combinations[$teamB][] = $teamA;
+                $teamB = $leagueTeams->get($j)->id;
+                $combinations[] = compact('teamA', 'teamB');
             }
         }
 
-        dd($combinations);
+        $rounds = array_fill(0, $size - 1, []);
+        $i = 0;
+        foreach ($combinations as $index => $combination) {
+            $allocated = false;
+            do {
+                if (count($rounds[$i]) < $size/2) {
+                    for ($j = 0, $found = false; $j < count($rounds[$i]) && !$found; $j++) {
+                        if ($rounds[$i][$j]['teamA'] == $combination['teamA']
+                            || $rounds[$i][$j]['teamB'] == $combination['teamA']
+                            || $rounds[$i][$j]['teamA'] == $combination['teamB']
+                            || $rounds[$i][$j]['teamB'] == $combination['teamB']) {
+                            $found = true;
+                        }
+                    }
+
+                    if (!$found) {
+                        $rounds[$i][] = $combination;
+                        unset($combinations[$index]);
+                        $allocated = true;
+                    }
+                }
+                $i = (++$i < count($rounds)) ? $i : 0;
+            } while(!$allocated);
+        }
+
+        dd($rounds);
 
         return $this->classificatoryConfrontation::class;
     }
